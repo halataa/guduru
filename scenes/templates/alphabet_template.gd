a64bf435d9969a4d62d17da4f32bc569e6770355
Ali Halataee <halataa@gmail.com>
2023-08-05 18:22:31 +0330

    new resolution

--- scenes/templates/alphabet_template.gd
+++ scenes/templates/alphabet_template.gd
@@ -11,12 +11,11 @@ var window_size = DisplayServer.window_get_size()
 
 func _process(delta):
 	var direction = 0
-	if Input.is_action_just_pressed("right"):
+	if Input.is_action_just_pressed("right") and !$RightRay.is_colliding():
 		direction = 1
-	elif Input.is_action_just_pressed("left"):
+	elif Input.is_action_just_pressed("left") and !$LeftRay.is_colliding():
 		direction = -1
-	if !$LeftRay.is_colliding() and !$RightRay.is_colliding():
-		move_alphabet(direction)
+	move_alphabet(direction)
 	if Input.is_action_pressed("fast"):
 		velocity.y = fast_v_y * delta
 	else:
